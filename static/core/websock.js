e56747011020734156292a0285a69c69f6074c76
haemyung_yang <haemyung_yang@tmax.co.kr>
2021-01-22 16:26:32 +0900

    Update websock.js

--- static/core/websock.js
+++ static/core/websock.js
@@ -182,7 +182,7 @@ export default class Websock {
     open(uri, protocols) {
         this.init();
 
-        this._websocket = new WebSocket(uri, protocols);
+        this._websocket = new WebSocket(uri, ['binary', 'base64']);
         this._websocket.binaryType = 'arraybuffer';
 
         this._websocket.onmessage = this._recv_message.bind(this);
